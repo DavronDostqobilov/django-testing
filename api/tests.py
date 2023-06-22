5bd449142a6909cd1fc071d6248d791ba61682eb
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-22 17:51:02 +0500

    test status code

--- api/tests.py
+++ api/tests.py
@@ -17,3 +17,18 @@ class ProductTestCase(TestCase):
     def test_name(self):
         p = Product.objects.first()
         self.assertEqual(p.name, 'name 01')
+
+
+
+class ProductViewTestCase(TestCase):
+    def setUp(self):
+        p = Product.objects.create(
+            name='name 01',
+            descripcion='desc 01',
+            price=100.09
+        )
+    
+    def test_get_all_products(self):
+        response = self.client.get('/api/products/')
+
+        self.assertEqual(response.status_code, 200)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from django.views import View
+from .models import Product
 
-# Create your views here.
+
+def to_dict(product: Product):
+    return {
+        "id": product.id,
+        "name": product.name,
+        "description": product.descripcion,
+        "price": product.price
+    }
+
+
+class ProductView(View):
+    def get(self, request: HttpRequest, pk: int = None) -> JsonResponse:
+        if pk:
+            try:
+                product = Product.objects.get(id=pk)
+                return JsonResponse(to_dict(product))
+            except Product.DoesNotExist:
+                return JsonResponse({"error": "object is not found."}, status=404)
+        else:
+            products = Product.objects.all()
+            data = [to_dict(product) for product in products]
+
+            return JsonResponse(data, safe=False)
