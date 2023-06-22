d58301515a4ad0f08274e65799129c93a58daef5
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-22 17:15:40 +0500

    test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+    def test_is_created(self):
+        p1 = Product.objects.create(
+            name='name 01',
+            descripcion='desc 01',
+            price=100.09
+        )
+        p2 = Product.objects.create(
+            name='name 02',
+            descripcion='desc 02',
+            price=140.90
+        )
+
+        count = Product.objects.all().count()
+
+        self.assertEqual(count, 2)
